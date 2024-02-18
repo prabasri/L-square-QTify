ae24ed898b591fe5df2fa0046dd8002ba1646030
Praba sri <prabasri1998@gmail.com>
2024-02-18 19:13:18 +0530

    Added Poppins font

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoImage from '../../Assets/logo.png';
+import styles from "./Logo.module.css"
 
 export default function Logo() {
-  return <img className={StyleSheet.Logo} src={LogoImage} alt="qtify-logo" width={67} height={34} />
+  return <img className={styles.Logo} src={LogoImage} alt="qtify-logo" width={67} height={34} />
 }
